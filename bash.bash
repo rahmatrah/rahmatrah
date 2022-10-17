1d1e8d14c92d1ae369822701bf5ceffa1084b6ea
rahmatrah <79177373+rahmatrah@users.noreply.github.com>
2022-10-17 13:02:11 +0700

    Create bash.bash

---
+++ bash.bash
@@ -0,0 +1,15 @@
+command_not_found_handle() {
+        /data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+}
+PS1='\[\e[31m\]┌─[\[\e[37m\]\T\[\e[31m\]]─────\e[1;93m[root]\e[0;31m───[\#]\n|\n\e[0;31m└─[\[\e[31m\]\e[0;35m\W\[\e[31m\]]────►\e[1;92m'
+clear
+mpv /$HOME/TERMUX-WELCOME/terminal.mp3
+clear
+echo -e "Created By github.com/itzr00tbyte" | lolcat
+echo "----------------------------" | lolcat
+echo "BY script kiddie Youtube channel " | lolcat
+figlet -c Welcome  
+date | lolcat
+echo -e "YOUTUBE CHANNEL  : \n www.youtube.com/c/scriptkiddietelugu" | lolcat
+echo "-----------------------------------" | lolcat
+echo "Follow Me On Instagram :itz_r00tbyte" | lolcat
